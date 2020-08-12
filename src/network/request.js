56cbc1351df7567a624e449eb7ab44f87096eb68
Codehsder <2323671700@qq.com>
2020-08-12 14:12:23 +0800

    修改

--- src/network/request.js
+++ src/network/request.js
@@ -3,18 +3,18 @@ export function request(config){
     //使用自定义配置创建axios实例，可在其他页面使用
     const instance = axios.create({
         //urlbase:'http://123.207.32.32:8000'
-        baseURL:'http://152.136.185.210:8000/api/n3',
+        baseURL:'http://152.136.185.210:8000/api/z8',
         timeout:5000
    })
 
 //axiso拦截器
-//响应拦截
+//请求拦截
 instance.interceptors.request.use(config=>{
     return config
 }),err=>{
 
 }
-
+//响应拦截
 instance.interceptors.response.use(res=>{
     return res.data
 }),err=>{
